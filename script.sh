7e004aa20aad0b8a7dd3e89f49eab487844dfca6
Matthias Malzahn <matthias@malzahn.kim>
2025-01-13 14:01:07 +0700

    update

--- script.sh
+++ script.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-
 filecontent=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq '.filecontent')
-
 filename=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq '.filename')
-
 filepath="${MOUNTED_IAC_SOURCE_CODE_DIR}/$filename"
-
 #printf "%s" "$filecontent" > "$filepath" 2>/dev/null;
-
 printf 'Filecontent \n######################\n'
 printf $filecontent
 printf "\n\nFilename \n######################\n"
