2d063f97710c18bdf2be7f400a79da2a979ebff5
Matthias Malzahn <matthias@malzahn.kim>
2025-01-13 13:01:13 +0700

    update script

--- script.sh
+++ script.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
 
+filecontent=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq '.filecontent')
 
-if [ -n "$SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES" ]; then
-  echo "Decoding SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES:"
-  echo "$SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES" | base64 -d | jq
-else
-  echo "SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES is not set."
-fi
+filename=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq '.filename')
 
-echo ""
+filepath="${MOUNTED_IAC_SOURCE_CODE_DIR}/$filename"
 
-if [ -n "$SG_BASE64_IAC_INPUT_VARIABLES" ]; then
-  echo "Decoding SG_BASE64_IAC_INPUT_VARIABLES:"
-  echo "$SG_BASE64_IAC_INPUT_VARIABLES" | base64 -d | jq
-else
-  echo "SG_BASE64_IAC_INPUT_VARIABLES is not set."
-fi
-
-ls -l $MOUNTED_IAC_SOURCE_CODE_DIR
-
-cat $MOUNTED_IAC_SOURCE_CODE_DIR/backend.tf
-
-echo "\n Trenner \n\n"
-
-cat $MOUNTED_IAC_SOURCE_CODE_DIR/provider.tf
+#printf "%s" "$filecontent" > "$filepath" 2>/dev/null;
+echo $filepath
