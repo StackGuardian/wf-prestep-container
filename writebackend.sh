eaadee83962d73b2c75a68bb0dc61af70bf54077
Matthias Malzahn <matthias@malzahn.kim>
2025-02-26 13:31:31 +0700

    add backend file creation script

---
+++ writebackend.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+resource_group_name=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq -r '.resource_group_name')
+storage_account_name=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq -r '.storage_account_name')
+container_name=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq -r '.container_name')
+key=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq -r '.statefilename')
+filepath_backend="${MOUNTED_IAC_SOURCE_CODE_DIR}"/backend.tf"
+
+read -r -d '' backendcontent << EOF
+terraform {
+  backend "azurerm" {
+    resource_group_name  = $resource_group_name
+    storage_account_name = $storage_account_name
+    container_name       = $container_name
+    key                  = $statefilename
+  }
+}
+EOF
+
+printf "%b" "$backendcontent" > "$filepath_backend" 2>/dev/null;
