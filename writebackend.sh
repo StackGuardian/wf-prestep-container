6f3c0ef4fb5298634e12803764c5cd87ca8ae729
Matthias Malzahn <matthias@malzahn.kim>
2025-03-20 16:23:16 +0700

    change back to HCL format

--- writebackend.sh
+++ writebackend.sh
@@ -4,19 +4,15 @@ resource_group_name=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --d
 storage_account_name=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq -r '.storage_account_name')
 container_name=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq -r '.container_name')
 statefilename=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq -r '.statefilename')
-filepath_backend=${MOUNTED_IAC_SOURCE_CODE_DIR}"/backend.tf.json"
+filepath_backend=${MOUNTED_IAC_SOURCE_CODE_DIR}"/backend.tf"
 
 read -r -d '' backendcontent << EOF
-{
-"terraform": {
-    "backend": {
-      "azurerm": {
-          "resource_group_name": "$resource_group_name",
-          "storage_account_name": "$storage_account_name",
-          "container_name": "$container_name",
-          "key": "$statefilename"
-          }
-      }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "$resource_group_name"
+    storage_account_name = "$storage_account_name"
+    container_name       = "$container_name"
+    key                  = "$statefilename"
   }
 }
 EOF
