09999e7f5daa2126059f43c228ea19b726b57341
Matthias Malzahn <matthias.malzahn@stackguardian.io>
2025-04-07 12:44:41 +0700

    add docu to the script

--- scripts/write_az_backend.sh
+++ scripts/write_az_backend.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+################################################################################
+# Input paramters which must be set in the template
+#
+# resource_group_name -> Name of the Resource Group in which the Storage Account
+#                        is located
+# storage_account_name -> Name of the Storage Account
+# container_name -> Name of the container iun the Storage Account
+# statefilename -> Name of the state file, including the "path"
+################################################################################
+
 # Write the backend configuration to backend.tf
 
 resource_group_name=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq -r '.resource_group_name')
