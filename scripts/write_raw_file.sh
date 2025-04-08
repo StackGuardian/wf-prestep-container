f74a701f50f243dabe9b6d0e22d67c3928faffbf
Matthias Malzahn <matthias.malzahn@stackguardian.io>
2025-04-08 19:20:49 +0700

    add comments in file

--- scripts/write_raw_file.sh
+++ scripts/write_raw_file.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Writing the raw file to the mounted directory
+################################################################################
+# Input paramters which must be set in the template
+#
+# filecontent -> The content of the file to be written
+# filename -> The name of the file to be written
+################################################################################
+
 filecontent=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq -r '.filecontent')
 filename=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq -r '.filename')
 filepath="${MOUNTED_IAC_SOURCE_CODE_DIR}"/"${filename}"
