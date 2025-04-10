e07fc2c7d920990a663ea74aad8f735438391c1a
Matthias Malzahn <matthias.malzahn@stackguardian.io>
2025-04-10 20:36:28 +0700

    add WFS for writing a raw file

--- scripts/write-raw-file.sh
+++ scripts/write-raw-file.sh
@@ -10,6 +10,7 @@
 
 filecontent=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq -r '.filecontent')
 filename=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq -r '.filename')
+
 filepath="${MOUNTED_IAC_SOURCE_CODE_DIR}"/"${filename}"
 
 printf "%b" "$filecontent" > "$filepath" 2>/dev/null;
