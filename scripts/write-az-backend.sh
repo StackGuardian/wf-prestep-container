497543fdad0ff4d1689cfbc66bc4e76c44a17788
Matthias Malzahn <matthias.malzahn@stackguardian.io>
2025-04-10 19:13:26 +0700

    add the posibility to set values by env variables

--- scripts/write-az-backend.sh
+++ scripts/write-az-backend.sh
@@ -12,10 +12,34 @@
 
 # Write the backend configuration to backend.tf
 
-resource_group_name=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq -r '.resource_group_name')
-storage_account_name=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq -r '.storage_account_name')
-container_name=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq -r '.container_name')
-statefilename=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq -r '.statefilename')
+# Check if the resource group name is set in the environment variable or in the input variables
+if [ -v BACKEND_RESOURCE_GROUP_NAME ]; then
+  resource_group_name=$BACKEND_RESOURCE_GROUP_NAME
+else
+  resource_group_name=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq -r '.resource_group_name')
+fi
+
+# Check if the storage account name is set in the environment variable or in the input variables
+if [ -v BACKEND_STORAGE_ACCOUNT_NAME ]; then
+  storage_account_name=$BACKEND_STORAGE_ACCOUNT_NAME
+else
+  storage_account_name=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq -r '.storage_account_name')
+fi
+
+# Check if the container name is set in the environment variable or in the input variables
+if [ -v BACKEND_CONTAINER_NAME ]; then
+  container_name=$BACKEND_CONTAINER_NAME
+else
+  container_name=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq -r '.container_name')
+fi
+
+# Check if the statefilename is set in the environment variable or in the input variables
+if [ -v BACKEND_STATE_FILENAME ]; then
+  statefilename=$BACKEND_STATE_FILENAME
+else
+  statefilename=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq -r '.statefilename')
+fi
+
 filepath_backend=${MOUNTED_IAC_SOURCE_CODE_DIR}"/backend.tf"
 
 read -r -d '' backendcontent << EOF
