9d636c7d251d2fce9bee518d5ed567717b0081ff
Matthias Malzahn <matthias.malzahn@ext.fresenius.com>
2026-03-29 07:15:59 +0000

    update or migration

---
+++ scripts/migrate-az-backend.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+################################################################################
+# Input paramters which must be set in the template
+#
+# resource_group_name -> Name of the Resource Group in which the Storage Account
+#                        is located
+# storage_account_name -> Name of the Storage Account
+# container_name -> Name of the container iun the Storage Account
+# statefilename -> Name of the state file, including the "path"
+################################################################################
+
+# Write the backend configuration to backend.tf
+
+echo "Migrating existing state from Azure Storage Account backend to local state file..."
+echo "current files in ${MOUNTED_IAC_SOURCE_CODE_DIR}:"
+ls -la ${MOUNTED_IAC_SOURCE_CODE_DIR}
+
+echo "***************************************************************"
+
+
+# Check if the resource group name is set in the environment variable or in the input variables
+if [ -v BACKEND_RESOURCE_GROUP_NAME ]; then
+  resource_group_name=$BACKEND_RESOURCE_GROUP_NAME
+else
+  resource_group_name=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq -r '.resource_group_name')
+fi
+
+# Check if the storage account name is set in the environment variable or in the input variables
+if [ -v BACKEND_STORAGE_ACCOUNT_NAME ]; then
+  storage_account_name=$BACKEND_STORAGE_ACCOUNT_NAME
+else
+  storage_account_name=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq -r '.storage_account_name')
+fi
+
+# Check if the container name is set in the environment variable or in the input variables
+if [ -v BACKEND_CONTAINER_NAME ]; then
+  container_name=$BACKEND_CONTAINER_NAME
+else
+  container_name=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq -r '.container_name')
+fi
+
+# Check if the statefilename is set in the environment variable or in the input variables
+
+if [ -v BACKEND_STATE_FILENAME ]; then
+  statefilename=$BACKEND_STATE_FILENAME
+elif ["$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq -r '.use_default_statefile')"=="false"]; then
+  statefilename=$(echo $SG_BASE64_WORKFLOW_STEP_INPUT_VARIABLES | base64 --decode | jq -r '.statefilename')
+else
+  statefilename=${SG_WORKFLOW_ID}
+fi
+
+# Download the terraform state file from the backend storage account
+filepath_statefile=${MOUNTED_IAC_SOURCE_CODE_DIR}"/terraform.tfstate"
+
+echo "Downloading state file '${statefilename}' from storage account '${storage_account_name}' container '${container_name}'..."
+
+# Authenticate using OIDC token
+az login --federated-token "$SG_OIDC_TOKEN" --service-principal \
+  --username "$ARM_CLIENT_ID" --tenant "$ARM_TENANT_ID"
+
+az storage blob download \
+  --account-name "$storage_account_name" \
+  --container-name "$container_name" \
+  --name "$statefilename" \
+  --file "$filepath_statefile" \
+  --auth-mode login
+
+echo "AFTER: current files in ${MOUNTED_IAC_SOURCE_CODE_DIR}:"
+ls -la ${MOUNTED_IAC_SOURCE_CODE_DIR}
+
+echo "***************************************************************"
+
+
+
+if [ $? -eq 0 ]; then
+  echo "State file downloaded successfully to ${filepath_statefile}"
+else
+  echo "ERROR: Failed to download state file" >&2
+  exit 1
+fi
